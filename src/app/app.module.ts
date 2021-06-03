2f3194ab508688ce3dd11441c4ede3d87194ddf1
sasangachathumal <chathumal.sasanga02@gmail.com>
2021-06-03 10:37:03 +0530

    Build to Github Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OverviewPageComponent } from './overview-page/overview-page.component';
+import { MainIntroComponent } from './main-intro/main-intro.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkilsComponent } from './my-skils/my-skils.component';
+import { WorkingExpreanceComponent } from './working-expreance/working-expreance.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OverviewPageComponent
+    OverviewPageComponent,
+    MainIntroComponent,
+    AboutMeComponent,
+    MySkilsComponent,
+    WorkingExpreanceComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
